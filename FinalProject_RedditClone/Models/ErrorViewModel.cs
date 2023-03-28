2ec67b3fe38928301e1c69e41ebfae5ab5476f82
seanburkard <seanburkard@SEANBURKARD4A04>
2023-03-28 18:22:55 -0500

    Add project files.

---
+++ FinalProject_RedditClone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinalProject_RedditClone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
