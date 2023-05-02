d8293b7c0eaff716cf1b554f3a5a57b3c1d2311f
SeanBurkard <skburkard@crimson.ua.edu>
2023-05-02 17:54:48 -0500

    Added moderation capability

---
+++ FinalProject_RedditClone/Controllers/ModerationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+using OpenAI_API.Moderation;
+
+namespace FinalProject_RedditClone.Controllers
+{
+    [Route("UseChatGpt")]
+    [ApiController]
+    public class ModerationController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<Result> UseChatGpt(string query)
+        {
+            var openai = new OpenAIAPI("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");
+            ModerationRequest moderationRequest = new ModerationRequest();
+            moderationRequest.Input = query;
+
+            var moderation = openai.Moderation.CallModerationAsync(moderationRequest);
+
+            var result = moderation.Result.Results[0];
+
+            return result;
+        }
+    }
+}
